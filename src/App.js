6ea61e7cff88e02de5ac5fd93ad6888b96df0fe8
siinemvarol <siinemvarol@gmail.com>
2023-08-18 18:22:05 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import {useState, useEffect} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  const buttons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const [posts, setPosts] = useState(null);
+  const [buttonId, setButtonId] = useState(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const onClick = (id) => {    
+    setButtonId(id);    
+  }
+  useEffect(()=>{
+    if(buttonId !== null){
+      setIsVisible(true);
+      axios.get(`https://jsonplaceholder.typicode.com/posts/${buttonId}`)
+      .then((res) => {setPosts(res.data); 
+        setIsVisible(false);
+      })
+      //.finally(() => setIsVisible(false));
+    }
+   
+  },[buttonId]);
+
+  return ( 
+    <>
+       <div>
+    <h2>Data Fetching App</h2>
+    {buttons.map((button, index) => <button onClick={() => onClick(index+1)} key={index+1}>Button{button}</button>) 
+
+    }
+    {
+      isVisible ? (<p>Loading...</p>) : posts ? (<div>
+        <h3>Post {posts.id}</h3>
+        <p>{posts.title}</p>
+        <p>{posts.body}</p>
+      </div>
+      ): null
+    }
+      </div>  
+    </>    
   );
 }
 
